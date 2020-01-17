b1471025bd3943d868b57413d19e6fb8d31ec590
Fabio Rodrigues Pereira <faro@nmbu.no>
2020-01-17 23:36:02 +0100

    Pytestes for simulation implemented

--- src/biosim/fauna.py
+++ src/biosim/fauna.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-import random
-import numpy as np
 
 """
 This is the fauna model which functions with the BioSim package 
@@ -10,6 +8,9 @@ written for the INF200 project January 2019.
 __author__ = "Fábio Rodrigues Pereira and Rabin Senchuri"
 __email__ = "fabio.rodrigues.pereira@nmbu.no and rabin.senchuri@nmbu.no"
 
+import random
+import numpy as np
+
 
 class Population:
     parameters = {}
@@ -18,14 +19,14 @@ class Population:
     def fitness_formula(sgn, x, xhalf, phi):
         return 1.0 / (1 + np.exp(sgn * phi * (x - xhalf)))
 
-    @classmethod
+    @classmethod  # tested
     def check_unknown_parameters(cls, params):
         for parameter in params.keys():
             if parameter not in cls.parameters.keys():
                 raise ValueError("Unknown parameter provided: "
                                  "*{}*".format(parameter))
 
-    @classmethod
+    @classmethod  # tested
     def set_parameters(cls, params):
         cls.check_unknown_parameters(params)
         cls.parameters.update(params)
@@ -89,22 +90,11 @@ class Population:
 
 
 class Herbivore(Population):
-    parameters = {'w_birth': 8.0,
-                  'sigma_birth': 1.5,
-                  'beta': 0.9,
-                  'eta': 0.05,
-                  'a_half': 40.,
-                  'phi_age': 0.2,
-                  'w_half': 10.,
-                  'phi_weight': 0.1,
-                  'mu': 0.25,
-                  'lambda': 1,
-                  'gamma': 0.2,
-                  'zeta': 3.5,
-                  'xi': 1.2,
-                  'omega': 0.4,
-                  'F': 10.0,
-                  'DeltaPhiMax': None}
+    parameters = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9,
+                  'eta': 0.05, 'a_half': 40.0, 'phi_age': 0.2,
+                  'w_half': 10., 'phi_weight': 0.1, 'mu': 0.25,
+                  'lambda': 1, 'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2,
+                  'omega': 0.4, 'F': 10.0, 'DeltaPhiMax': None}
 
     def __init__(self, age=0, weight=None):
         super().__init__(age, weight)
@@ -115,22 +105,11 @@ class Herbivore(Population):
 
 
 class Carnivore(Population):
-    parameters = {'w_birth': 8.0,
-                  'sigma_birth': 1.5,
-                  'beta': 0.9,
-                  'eta': 0.05,
-                  'a_half': 40.,
-                  'phi_age': 0.2,
-                  'w_half': 10.,
-                  'phi_weight': 0.1,
-                  'mu': 0.25,
-                  'lambda': 1,
-                  'gamma': 0.2,
-                  'zeta': 3.5,
-                  'xi': 1.2,
-                  'omega': 0.4,
-                  'F': 10.0,
-                  'DeltaPhiMax': 10.0}
+    parameters = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9,
+                  'eta': 0.05, 'a_half': 40., 'phi_age': 0.2,
+                  'w_half': 10., 'phi_weight': 0.1, 'mu': 0.25,
+                  'lambda': 1, 'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2,
+                  'omega': 0.4, 'F': 10.0, 'DeltaPhiMax': 10.0}
 
     def __init__(self, age=0, weight=None):
         super().__init__(age, weight)
@@ -155,5 +134,4 @@ class Carnivore(Population):
 
                 if feed >= self.parameters['F']:
                     break
-        self.gain_weight(feed)
-        self.update_fitness()
+        self.gain_weight(feed), self.update_fitness()

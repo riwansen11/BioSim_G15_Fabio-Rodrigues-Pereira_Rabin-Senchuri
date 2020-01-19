9237421fd3b28ae277be80086ec385b5f10901cb
Fabio Rodrigues Pereira <faro@nmbu.no>
2020-01-19 13:16:45 +0100

    Carnivore eating rules corrected

--- fabiostestes.py
+++ fabiostestes.py
@@ -13,7 +13,7 @@ ini_herb = [
      "pop": [
          {"species": "Herbivore", "age": rd.randint(1, 10), "weight":
              rd.randint(20, 60)}
-         for _ in range(200)
+         for _ in range(20000)
      ]}]
 
 ini_carn = [
@@ -21,7 +21,7 @@ ini_carn = [
      "pop": [
          {"species": "Carnivore", "age": rd.randint(1, 10), "weight":
              rd.randint(20, 60)}
-         for _ in range(200)
+         for _ in range(20)
      ]}]
 t = BioSim(island_map, ini_herb, None)
 t.add_population(ini_carn)
@@ -56,4 +56,38 @@ print('carn_1_age:', carn_1_age)
 print('carn_1_weight:', carn_1_weight)
 print('carn_1_fitness:', carn_1_fitness)
 
-t.simulate(1)'''
+t.simulate(1)
+
+
+
+
+global herb_object, h_w, h_f, c_food_desired
+        self.population['Carnivore'].sort(key=lambda h: h.fitness,
+                                          reverse=True)
+        self.population['Herbivore'].sort(key=lambda h: h.fitness)
+        for carn_object in self.population["Carnivore"]:
+            c_appetite = carn_object.parameters['F']
+            c_ate = 0
+            c_food_desired = c_appetite - c_ate
+
+            while not c_food_desired <= 0:
+                for herb_object in self.population['Herbivore']:
+                    h_w = herb_object.weight
+                    h_f = herb_object.fitness
+                    if carn_object.is_herb_killed(h_f):
+                        print('Killed:')
+                        print('Food desired', c_food_desired)
+                        if h_w <= c_food_desired:
+                            print('h_weight <= c_food_desired', h_w, c_food_desired)
+                            carn_object.gain_weight(h_w)
+                            carn_object.update_fitness()
+                            c_food_desired -= h_w
+                            print('killed', herb_object)
+                            self.population["Herbivore"].remove(herb_object)
+                        elif h_w > c_food_desired:
+                            print('h_weight > c_food_desired', h_w, c_food_desired)
+                            carn_object.gain_weight(c_food_desired)
+                            carn_object.update_fitness()
+                            c_food_desired -= c_food_desired
+                            print('killed', herb_object)
+                            self.population["Herbivore"].remove(herb_object)'''
